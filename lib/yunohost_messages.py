ebf1e7d6844bb48dccbbac7fa314965fa61b829a
Kloadut <kload@kload.fr>
2012-10-07 18:00:28 +0200

    messages + gitignore

---
+++ lib/yunohost_messages.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+error       = "\033[31m\033[1m" + _("Error:") + "\033[m "
+interrupt   = "\033[31m\033[1m" + _("Interrupt:") + "\033[m "
+notice      = "\033[34m\033[1m" + _("Notice:") + "\033[m "
+success     = "\033[33m\033[1m" + _("Success:") + "\033[m "
