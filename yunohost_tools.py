bf5126af26b0049075bf0701ad9eb805de5ad478
Kloadut <kload@kload.fr>
2012-10-23 18:10:39 +0200

    Init function without admin mod

--- yunohost_tools.py
+++ yunohost_tools.py
@@ -2,12 +2,14 @@
 
 import yaml
 
-def tools_init(args, connections):
-    
+def tools_init(args, connections): 
     yldap = connections['ldap']
     
     with open('ldap_scheme.yml') as f: 
         ldap_map = yaml.load(f)
 
-    for rdn, attr_dict in ldap_map:
+    for rdn, attr_dict in ldap_map['parents'].items():
+        yldap.add(rdn, attr_dict)
+
+    for rdn, attr_dict in ldap_map['childs'].items():
         yldap.add(rdn, attr_dict)
