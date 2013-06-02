1d052ab5c3a326d09a2c61d3bf7045d7ab70241c
Alexis Gavoty <kload@kload.fr>
2013-06-02 20:31:43 +0200

    Add user conf at main domain changing

--- yunohost_tools.py
+++ yunohost_tools.py
@@ -133,6 +133,7 @@ def tools_maindomain(old_domain, new_domain):
     command_list = [
         'cp /etc/yunohost/apache/templates/sso.fixed.conf   /etc/yunohost/apache/domains/' + new_domain + '.d/sso.fixed.conf', # add SSO apache conf dir to new domain conf
         'cp /etc/yunohost/apache/templates/admin.fixed.conf /etc/yunohost/apache/domains/' + new_domain + '.d/admin.fixed.conf',
+        'cp /etc/yunohost/apache/templates/user.fixed.conf  /etc/yunohost/apache/domains/' + new_domain + '.d/user.fixed.conf',
         '/usr/share/lemonldap-ng/bin/lmYnhMoulinette',
         '/etc/init.d/hostname.sh',
         'echo "01" > '+ tmp +'/ssl/yunoCA/serial',
