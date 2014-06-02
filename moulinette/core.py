7145039e891ac4532adb9610d9fc5a7a78a6b591
Jérôme Lebleu <jerome.lebleu@mailoo.org>
2014-06-02 20:48:34 +0200

    [fix] Encode translated string before formating it

--- moulinette/core.py
+++ moulinette/core.py
@@ -191,7 +191,7 @@ class Translator(object):
                 logging.warning("unknown key '%s' for locale '%s'" %
                         (key, self.default_locale))
                 return key
-        return value.format(*args, **kwargs).encode('utf-8')
+        return value.encode('utf-8').format(*args, **kwargs)
 
     def _load_translations(self, locale, overwrite=False):
         """Load translations for a locale
