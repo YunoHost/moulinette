19b28d43d0539617510fde2b2ef63aa0b7478794
Jérôme Lebleu <jerome.lebleu@mailoo.org>
2014-07-28 22:04:09 +0200

    [fix] Catch error while closing stream in NonBlockingStreamReader

--- moulinette/helpers.py
+++ moulinette/helpers.py
@@ -52,4 +52,7 @@ class NonBlockingStreamReader:
 
     def close(self):
         """Close the stream"""
-        self._s.close()
+        try:
+            self._s.close()
+        except IOError:
+            pass
