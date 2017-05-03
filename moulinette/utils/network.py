8fbdf758880873c7f4259fcee94cee0dc1d143a2
Alexandre Aubin <alex.aubin@mailoo.org>
2017-05-02 20:42:12 -0400

    Moving Yunohost/#274 to Moulinette

---
+++ moulinette/utils/network.py
@@ -0,0 +1,61 @@
+import errno
+import requests
+import json
+
+from moulinette.core import MoulinetteError
+
+
+def download_text(url, timeout=30):
+    """
+    Download text from a url and returns the raw text
+
+    Keyword argument:
+        url -- The url to download the data from
+        timeout -- Number of seconds allowed for download to effectively start
+        before giving up
+    """
+    # Assumptions
+    assert isinstance(url, str)
+
+    # Download file
+    try:
+        r = requests.get(url, timeout=timeout)
+    # Invalid URL
+    except requests.exceptions.ConnectionError:
+        raise MoulinetteError(errno.EBADE,
+                              m18n.g('invalid_url', url=url))
+    # SSL exceptions
+    except requests.exceptions.SSLError:
+        raise MoulinetteError(errno.EBADE,
+                              m18n.g('download_ssl_error', url=url))
+    # Timeout exceptions
+    except requests.exceptions.Timeout:
+        raise MoulinetteError(errno.ETIME,
+                              m18n.g('download_timeout', url=url))
+    # Unknown stuff
+    except Exception as e:
+        raise MoulinetteError(errno.ECONNRESET,
+                              m18n.g('download_unknown_error',
+                                     url=url, error=str(e)))
+    # Assume error if status code is not 200 (OK)
+    if r.status_code != 200:
+        raise MoulinetteError(errno.EBADE,
+                              m18n.g('download_bad_status_code',
+                                     url=url, code=str(r.status_code)))
+
+    return r.text
+
+
+def download_json(url, timeout=30):
+
+    # Fetch the data
+    text = download_text(url, timeout)
+
+    # Try to load json to check if it's syntaxically correct
+    try:
+        loaded_json = json.loads(text)
+    except ValueError:
+        raise MoulinetteError(errno.EINVAL,
+                              m18n.g('corrupted_json', ressource=url))
+
+    return loaded_json
---
+++ moulinette/utils/tests/conftest.py
@@ -0,0 +1,96 @@
+import sys
+import moulinette
+
+sys.path.append("..")
+
+###############################################################################
+#   Tweak moulinette init to have yunohost namespace                          #
+###############################################################################
+
+
+old_init = moulinette.core.Moulinette18n.__init__
+def monkey_path_i18n_init(self, package, default_locale="en"):
+    old_init(self, package, default_locale)
+    self.load_namespace("moulinette")
+moulinette.core.Moulinette18n.__init__ = monkey_path_i18n_init
+
+
+###############################################################################
+#   Tweak translator to raise exceptions if string keys are not defined       #
+###############################################################################
+
+
+old_translate = moulinette.core.Translator.translate
+def new_translate(self, key, *args, **kwargs):
+
+    if key not in self._translations[self.default_locale].keys():
+        raise KeyError("Unable to retrieve key %s for default locale !" % key)
+
+    return old_translate(self, key, *args, **kwargs)
+moulinette.core.Translator.translate = new_translate
+
+def new_m18nn(self, key, *args, **kwargs):
+    return self._global.translate(key, *args, **kwargs)
+moulinette.core.Moulinette18n.g = new_m18nn
+
+
+###############################################################################
+#   Init the moulinette to have the cli loggers stuff                         #
+###############################################################################
+
+
+def pytest_cmdline_main(config):
+    """Configure logging and initialize the moulinette"""
+    # Define loggers handlers
+    handlers = set(['tty'])
+    root_handlers = set(handlers)
+
+    # Define loggers level
+    level = 'INFO'
+    tty_level = 'SUCCESS'
+
+    # Custom logging configuration
+    logging = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'tty-debug': {
+                'format': '%(relativeCreated)-4d %(fmessage)s'
+            },
+            'precise': {
+                'format': '%(asctime)-15s %(levelname)-8s %(name)s %(funcName)s - %(fmessage)s'
+            },
+        },
+        'filters': {
+            'action': {
+                '()': 'moulinette.utils.log.ActionFilter',
+            },
+        },
+        'handlers': {
+            'tty': {
+                'level': tty_level,
+                'class': 'moulinette.interfaces.cli.TTYHandler',
+                'formatter': '',
+            },
+        },
+        'loggers': {
+            'moulinette': {
+                'level': level,
+                'handlers': [],
+                'propagate': True,
+            },
+            'moulinette.interface': {
+                'level': level,
+                'handlers': handlers,
+                'propagate': False,
+            },
+        },
+        'root': {
+            'level': level,
+            'handlers': root_handlers,
+        },
+    }
+
+    # Initialize moulinette
+    moulinette.init(logging_config=logging, _from_source=False)
+
