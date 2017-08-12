5b4efe8efb59d9cde0b6df7a91ffbdd73fabdb1c
Laurent Peuch <cortex@worlddomination.be>
2017-08-12 17:18:56 +0200

    [doc] try this mock magic thingy for ldap

--- doc/conf.py
+++ doc/conf.py
@@ -21,6 +21,18 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 
+import sys
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['ldap']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
