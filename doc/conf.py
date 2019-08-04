f1ac5b8fe6be00cd47daca683ed599b0af33de2b
Laurent Peuch <cortex@worlddomination.be>
2019-08-04 19:05:33 +0200

    [doc/fix] fix doc building

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['ldap', 'ldap.modlist']
+MOCK_MODULES = ['ldap', 'ldap.modlist', 'ldap.sasl']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
