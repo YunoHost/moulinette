315db8ac71c73c708bdb993bcd4ee6ad472d086b
Kay0u <pierre.moltess@gmail.com>
2019-12-28 23:31:23 +0800

    more tests for actionmap

--- test/src/testauth.py
+++ test/src/testauth.py
@@ -2,9 +2,25 @@ def testauth_none():
     return "some_data_from_none"
 
 
+def testauth_subcat_none():
+    return "some_data_from_subcat_none"
+
+
 def testauth_default():
     return "some_data_from_default"
 
 
+def testauth_subcat_default():
+    return "some_data_from_subcat_default"
+
+
+def testauth_subcat_post():
+    return "some_data_from_subcat_post"
+
+
 def testauth_other_profile():
     return "some_data_from_other_profile"
+
+
+def testauth_subcat_other_profile():
+    return "some_data_from_subcat_other_profile"
